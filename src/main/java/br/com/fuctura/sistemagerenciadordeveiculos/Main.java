52f0281441d6702ef266d308eafed6633f6d9e5f
Weynne <wjgcl@cesar.school>
2024-08-15 17:06:06 -0300

    Main View e JavaFX no pom.xml

---
+++ src/main/java/br/com/fuctura/sistemagerenciadordeveiculos/Main.java
@@ -0,0 +1,32 @@
+package br.com.fuctura.sistemagerenciadordeveiculos;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			Scene mainScene = new Scene(parent);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sistema Gerenciador de Veículos");
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
