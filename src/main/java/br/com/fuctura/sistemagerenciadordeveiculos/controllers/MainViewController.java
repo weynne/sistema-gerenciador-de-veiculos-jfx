76c335c4e232e6b04e04947da02064323abd0241
Weynne <wjgcl@cesar.school>
2024-08-16 07:15:35 -0300

    Sobre View

--- src/main/java/br/com/fuctura/sistemagerenciadordeveiculos/controllers/MainViewController.java
+++ src/main/java/br/com/fuctura/sistemagerenciadordeveiculos/controllers/MainViewController.java
@@ -1,11 +1,20 @@
 package br.com.fuctura.sistemagerenciadordeveiculos.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import br.com.fuctura.sistemagerenciadordeveiculos.Main;
+import br.com.fuctura.sistemagerenciadordeveiculos.controllers.utils.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -38,12 +47,30 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemSobreAction() {
-		System.out.println("onMenuItemSobreAction");
+		loadView("/gui/Sobre.fxml");
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 
+	}
+
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro ao carregar a página", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
